c95884f0b5f57ec9e1d97c63f0f1a5ab547eb7c9
gustavosinbandera1 <gustavosinbandera1@hotmail.com>
2021-02-16 12:14:40 -0500

    remove unused code

--- main.cpp
+++ main.cpp
@@ -1,22 +1,17 @@
 #include <iostream>
-#include "state.h"
-#include "events.h"
-#include "EvDefault.h"
 #include "airConCtrl.h"
-#include "stateMode.h"
-#include "stateStopped.h"
-#include "airConCtrlCtx.h"
 #include "evPowerBtn.h"
 #include "EvOne.h"
 
+
+/*
+g++ main.cpp events.cpp EvDefault.cpp  state.cpp stateMachine.cpp parallelState.cpp stateCooler.cpp evModeBtn.cpp stateMode.cpp stateStopped.cpp  airConCtrl.cpp stateSpeed.cpp stateLow.cpp stateHigh.cpp  airConCtrlCtx.cpp EvOne.cpp evPowerBtn.cpp stateHeater.cpp stateOperating.cpp -o  test 
+*/
  
 
 int main(){
 AirConCtrl *air = new AirConCtrl();
-//AirConCtrlCtx *context = new AirConCtrlCtx();
-//air->dispatchEvent();
-
-EvPowerBtn evPowerBtn; //= new EvPowerBtn();
+EvPowerBtn evPowerBtn;
 EvOne evOne;
 
 air->dispatchEvent(&evPowerBtn);
